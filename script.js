37c18fbdf3db677b429f129865aea85277cf8ff7
Your Name <you@example.com>
2024-11-20 23:18:20 -0800

    added both color to html and to javascript

--- script.js
+++ script.js
@@ -2,7 +2,15 @@ const form = document.getElementById('forms')
 
 form.addEventListener('submit', function(event){
     event.preventDefault();
-    const inputValue = document.getElementById('userInput').ariaValueMax;
+    const inputValue = document.getElementById('userInput').value;
     console.log("Form submission prevented. Input value:", inputValue);
 });
+
+const colorInput = document.getElementById('colorInput');
+
+colorInput.addEventListener('input', function(event){
+    const colorValue = colorInput.value;
+
+    console.log("Color selected:", colorValue);
+})
 
